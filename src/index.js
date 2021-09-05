488b8efb30bc99c0954191d14fe49b29c1f12085
Kengathua <gathuakennedy@gmail.com>
2021-09-05 15:03:02 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './style.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light py-lg-0 py-3 px-0">
+                <a class="navbar-brand" href="index.html"><img src="/home/gathua/projects/Project/logo.svg" alt="" class="img-fluid" /></a>
+                <div class="collapse-navbar" id="navbarSupportedContent">
+                    <ul class="navbar-nav"> 
+                        <li><a class="navigation-element" href="#home"> Home </a></li>
+                        <li><a href="#"> About Us </a></li>
+                        <li><a href="#"> Contact </a></li>
+                        <li><a href="#third"> FAQ </a></li>
+                        <li><a href="#"> Services </a></li>
+                        <li><a href="#"> Compare </a></li>
+                        <li><a href="#"> Blog </a></li>
+                        <li><a href="#"> Sell </a></li>
+                    </ul>
+                </div>
+                <form class="search-form">
+                     <button class="search-button" type="submit" onclick="searchCar()">Search</button>
+                </form>
+            </nav>
+        </div>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
