c60dbf23d91c97c5d44bf5e33a67d49ed053ceff
Kengathua <gathuakennedy@gmail.com>
2021-09-05 16:40:13 +0300

    connect to api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, {Component} from 'react'
-import Table from './Table'
 
 class App extends Component {
-    render() {
-      const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]
-  
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      )
-    }
+  state = {
+    data: [],
   }
+
+  // Code is invoked after the component is mounted/inserted into the DOM tree.
+  componentDidMount() {
+    const url =
+      'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+
+    fetch(url)
+      .then((result) => result.json())
+      .then((result) => {
+        this.setState({
+          data: result,
+        })
+      })
+  }
+
+  render() {
+    const {data} = this.state
+
+    const result = data.map((entry, index) => {
+      return <li key={index}>{entry}</li>
+    })
+
+    return <ul>{result}</ul>
+  }
+}
 
 export default App
