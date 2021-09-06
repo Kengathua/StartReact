3821e221b470c46f4e0a3bc7e9c02d24738215db
Kengathua <gathuakennedy@gmail.com>
2021-09-07 01:45:47 +0300

    Calling a book separately

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const author = 'Admiral William H. McRaven';
-const title  = 'The Hero Code: Lessons Learned from Lives…';
-const img    = 'https://images-na.ssl-images-amazon.com/images/I/8123TQCpIXL._AC_UL200_SR200,200_.jpg'
+const firstBook={
+    author : 'Admiral William H. McRaven',
+    title  : 'The Hero Code: Lessons Learned from Lives…',
+    img    : 'https://images-na.ssl-images-amazon.com/images/I/8123TQCpIXL._AC_UL200_SR200,200_.jpg',
+}
+
 
 function BookList(){
     return (
         <section className="bookList"> 
-            <Book job='developer' />
+            <Book 
+            img={firstBook.img}
+            author={firstBook.author}
+            title = {firstBook.title}
+             />
             <Book title = 'random title' number={22} />
         </section>
         );
@@ -20,11 +27,9 @@ function BookList(){
 const Book =(props)=>{
     return(
         <article className="book">
-            <img src={img} alt='' />
-            <h1>{title}</h1>
-            <h4 style ={{color:'blue', fontSize:'0.75rem',marginTop:'0.25rem'}}>{author}</h4>
-            <p>{props.job}</p>
-            <p>{props.number}</p>
+            <img src={props.img} alt='' />
+            <h1>{props.title}</h1>
+            <h4 style ={{color:'blue', fontSize:'0.75rem',marginTop:'0.25rem'}}>{props.author}</h4>
         </article>
     );
 };
